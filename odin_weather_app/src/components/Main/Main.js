f3e371c28aaa7c032cccbd4507b008f514d4eeb9
Richard Walsh <richwalsh889@gmail.com>
2021-03-01 13:27:44 +0000

    initial style for searchbar and weather output

--- odin_weather_app/src/components/Main/Main.js
+++ odin_weather_app/src/components/Main/Main.js
@@ -5,7 +5,17 @@ import classes from './Main.module.css';
 const main = (props) => {
   return (
     <main className={classes.main}>
-      <div className={classes.weatherContainer}></div>
+      <div className={classes.innerContainer}>
+        <div className={classes.locationSearch}>
+          <input className={classes.locationSearchInput}></input>
+          <button className={classes.locationSearchBtn}>SEARCH</button>
+        </div>
+        <div className={classes.weatherContainer}>
+          <div className={classes.weatherLocation}></div>
+          <div className={classes.weatherLocation}></div>
+          <div className={classes.weatherLocation}></div>
+        </div>
+      </div>
     </main>
   );
 };
