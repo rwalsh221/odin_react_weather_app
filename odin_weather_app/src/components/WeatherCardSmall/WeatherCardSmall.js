976124c040c70e76e4c60ffb71fe50cb5562e7c6
Richard Walsh <richwalsh2@googlemail.com>
2021-08-23 12:21:57 +0100

    add position fixed animation to small weather card

--- odin_weather_app/src/components/WeatherCardSmall/WeatherCardSmall.js
+++ odin_weather_app/src/components/WeatherCardSmall/WeatherCardSmall.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './WeatherCardSmall.module.css';
 
 const WeatherCardSmall = (props) => {
+  const [elementPosition, setElementPosition] = useState();
+
   let style = props.hover
     ? `${classes.circleTop} ${classes.circleHover} ${classes.red}`
     : `${classes.circleTop}`;
-  return <div className={style} onClick={props.setHoverClass}></div>;
+
+  const getElementPosition = (element) => {
+    const elementPosition = element.target.getBoundingClientRect();
+
+    setElementPosition({
+      position: 'fixed',
+      top: elementPosition.top,
+      left: elementPosition.left,
+    });
+  };
+
+  return (
+    <div
+      style={elementPosition}
+      className={style}
+      onClick={(e) => {
+        getElementPosition(e);
+        props.setHoverClass();
+      }}
+    ></div>
+  );
 };
 
 export default WeatherCardSmall;
