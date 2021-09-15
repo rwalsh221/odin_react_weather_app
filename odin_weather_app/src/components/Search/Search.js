6203d4b4be562ad4327d325f74af41f8493d4b51
Richard Walsh <richwalsh2@googlemail.com>
2021-09-15 12:08:50 +0100

    move gridposition function to seperate file and create closure

--- odin_weather_app/src/components/Search/Search.js
+++ odin_weather_app/src/components/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import classes from './Search.module.css';
+import test from '../../utilities/gridposition';
 
 const Search = ({ addWeatherLocationProps }) => {
   const [location, setLocation] = useState();
@@ -20,7 +21,9 @@ const Search = ({ addWeatherLocationProps }) => {
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    addWeatherLocationProps(location, radioUnit, getGridPosition(), nanoid());
+
+    const gridPos = test();
+    addWeatherLocationProps(location, radioUnit, gridPos, nanoid());
   };
 
   let unavaliableGridPosition = [];
@@ -43,6 +46,32 @@ const Search = ({ addWeatherLocationProps }) => {
     }
     return gridPositionCSS[gridPositionNum - 1];
   };
+
+  // const gridPositionTest = () => {
+  //   let unavaliableGridPosition = [];
+  //   const getGridPos = () => {
+  //     //   GRID POSITION STRINGS FOR CSS STYLE
+  //     const gridPositionCSS = ['tl', 'tc', 'tr', 'cl', 'cr', 'bl', 'bc', 'br'];
+
+  //     //   GET RANDOM NUMBER FOR CSSPOSITION ARRAY
+  //     const gridPositionNum = Math.floor(Math.random() * 8) + 1;
+
+  //     if (unavaliableGridPosition.indexOf(gridPositionNum) === -1) {
+  //       unavaliableGridPosition.push(gridPositionNum);
+  //       console.log(unavaliableGridPosition);
+  //     } else if (unavaliableGridPosition.length >= 8) {
+  //       // DELETE FIRST ELEMENT IN LOCATIONS ARRAY AND REPLACE
+  //       console.log('arr complete');
+  //     } else {
+  //       getGridPos();
+  //     }
+  //     return gridPositionCSS[gridPositionNum - 1];
+  //   };
+
+  //   return getGridPos;
+  // };
+
+  // const test = gridPositionTest();
 
   return (
     <section className={classes.searchSection}>
--- odin_weather_app/src/utilities/gridposition.js
+++ odin_weather_app/src/utilities/gridposition.js
@@ -1,20 +1,33 @@
-let unavaliableGridPosition = [];
+// NEEDS PROMISE AND AWAIT. UPDATE STATE DOENT WAIT FOR RETURN IF IT NEEDS TO RUN AGIAN.
+const gridPositionTest = () => {
+  let unavaliableGridPosition = [];
+  const getGridPos = () => {
+    //   GRID POSITION STRINGS FOR CSS STYLE
+    const gridPositionCSS = ['tl', 'tc', 'tr', 'cl', 'cr', 'bl', 'bc', 'br'];
+    console.log(
+      'GENERTATE**************************************************************************************'
+    );
+    //   GET RANDOM NUMBER FOR CSSPOSITION ARRAY
+    const gridPositionNum = Math.floor(Math.random() * 8) + 1;
 
-const getGridPosition = () => {
-  //   GRID POSITION STRINGS FOR CSS STYLE
-  const gridPositionCSS = ['tl', 'tc', 'tr', 'cl', 'cr', 'bl', 'bc', 'br'];
+    if (unavaliableGridPosition.indexOf(gridPositionNum) === -1) {
+      console.log(unavaliableGridPosition.indexOf(gridPositionNum));
+      unavaliableGridPosition.push(gridPositionNum);
+      console.log(unavaliableGridPosition);
+      console.log(gridPositionNum);
+      console.log(gridPositionCSS[gridPositionNum - 1]);
+      return gridPositionCSS[gridPositionNum - 1];
+    } else if (unavaliableGridPosition.length >= 8) {
+      // DELETE FIRST ELEMENT IN LOCATIONS ARRAY AND REPLACE NEW FUNCTION
+      console.log('arr complete');
+    } else {
+      getGridPos();
+    }
+  };
 
-  //   GET RANDOM NUMBER FOR CSSPOSITION ARRAY
-  const gridPositionNum = Math.floor(Math.random() * 8) + 1;
-
-  if (unavaliableGridPosition.indexOf(gridPositionNum) === -1) {
-    unavaliableGridPosition.push(gridPositionNum);
-  } else if (unavaliableGridPosition.length >= 8) {
-    console.log('arr complete');
-  } else {
-    getGridPosition();
-  }
-  return gridPositionCSS[gridPositionNum - 1];
+  return getGridPos;
 };
 
-export default getGridPosition;
+const test = gridPositionTest();
+
+export default test;
