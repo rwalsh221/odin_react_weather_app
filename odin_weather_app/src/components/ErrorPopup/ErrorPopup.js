0aeec578b7cf26633d7a40f4cd08656fa8f5fd9d
Richard Walsh <richwalsh2@googlemail.com>
2021-10-04 14:57:46 +0100

    eslint error.js

--- odin_weather_app/src/components/ErrorPopup/ErrorPopup.js
+++ odin_weather_app/src/components/ErrorPopup/ErrorPopup.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './ErrorPopup.module.css';
 
-const ErrorPopup = ({ showErrorProps }) => {
-  return (
-    <div style={{ display: showErrorProps }} className={classes.errorPopup}>
-      <p>LOCATION NOT FOUND</p>
-    </div>
-  );
+const ErrorPopup = ({ showErrorProps }) => (
+  <div style={{ display: showErrorProps }} className={classes.errorPopup}>
+    <p>LOCATION NOT FOUND</p>
+  </div>
+);
+
+ErrorPopup.propTypes = {
+  showErrorProps: PropTypes.string.isRequired,
 };
 
 export default ErrorPopup;
